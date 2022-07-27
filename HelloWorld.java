217b98a8a1e9b43072aa6368a327104423f0b8bf
jawahar10 <66983940+srt-jawahar@users.noreply.github.com>
2022-07-27 09:54:30 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, This is World!"); 
+        System.out.println("Hello, This is Poll SCM Examples!"); 
     }
 }
